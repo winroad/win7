5c63d26a0f160ae8e1fd3819cb900439938f9b6d
Hidemasa Nakada <nakada@winroad.jp>
2019-03-11 07:24:37 +0900

    GPSコマンドの修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,40 +48,50 @@
         </div>
     </div>
 </div>
-    <div class="container">
-        <h1 style="text-align:center;color:#d36015;">[getCurrentPosition()]のサンプルデモ (Geolocation API)</h1>
-        <p>Geolocation APIの[getCurrentPosition()]を利用して、ユーザーの現在位置を取得するサンプルデモです。</p>
-
-        <HR style="margin: 3em 0 ;">
-
-        <h2>取得したデータ</h2>
-
-        <p>下記の位置情報を取得することができました。</p>
-
-        <dl id="result"></dl>
-
-        <h2>地図</h2>
-
-        <p>Google Mapsに、位置情報を反映させたものです。</p>
-
-        <div class="map-wrapper">
-            <div id="map-canvas"></div>
-        </div>
-
-
-        <HR style="margin: 3em 0 ;">
-
-
-
-        {{--<p style="text-align:center"><a href="https://syncer.jp/how-to-use-geolocation-api">配布元: Syncer</a></p>--}}
-
-
-
-
-        <!-- JavaScriptの読み込み -->
-        {{--<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB7Pxqp7Asgi6D4eOtkCxnir5xPiIwY3EE"></script>--}}
-        {{--<script src="{{ asset('js/gps.jp') }}"></script>--}}
-        {{--<script src="get-current-position.js"></script>--}}
-    </div>
-
+<script>
+    if( navigator.geolocation ){    // 現在位置を取得できる場合の処理
+        // 現在位置を取得する
+        navigator.geolocation.getCurrentPosition( success, error, option);
+        /*現在位置が取得できた時に実行*/
+        function success(position){
+            var data = position.coords;
+            // 必要な緯度経度だけ取得
+            var lat = data.latitude;
+            var lng = data.longitude;
+            // alert("緯度:" + lat + "  軽度:" + lng);
+                //Google Mapsで住所を取得
+                var geocoder = new google.maps.Geocoder();
+                latlng = new google.maps.LatLng(lat, lng);
+                geocoder.geocode({'latLng': latlng}, function(results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        document.getElementById('address').innerHTML = results[0].formatted_address;
+                        alert("緯度:" + lat + "  軽度:" + lng);
+                    }
+                    else {
+                        alert("エラー" + status);
+                    }
+                });
+            }
+            /*現在位置の取得に失敗した時に実行*/
+            function error(error){
+                var errorMessage = {
+                    0: "原因不明のエラーが発生しました。",
+                    1: "位置情報が許可されませんでした。",
+                    2: "位置情報が取得できませんでした。",
+                    3: "タイムアウトしました。",
+                } ;
+                //とりあえずalert
+                alert( errorMessage[error.code]);
+            }
+            // オプション(省略可)
+            var option = {
+                "enableHighAccuracy": false,
+                "timeout": 100 ,
+                "maximumAge": 100 ,
+            } ;
+        } else {// 現在位置を取得できない場合の処理
+        //とりあえずalert
+            alert("あなたの端末では、現在位置を取得できません。");
+        }
+</script>
 @endsection
